9fee4e9b55cbe128fdbc3285d9897a1ab5c992db
Dmitriy Sereda <nadechke@gmail.com>
2023-09-27 18:39:22 +0300

    Задание 41

---
+++ Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int AboveZeroNumsCount(int[] Array)
+{
+    int Length = Array.Length;
+    int Count = 0;
+    for(int i = 0; i < Length; i++)
+    {
+        if (Array[i] > 0)
+        {
+            Count++;
+        }
+    }
+    return Count;
+}
+
+int[] GetNum(string M)
+{
+    Console.Write($"Введите количество элементов {M} / ");
+    int ArrayLength = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("");
+    int[] Array = new int[ArrayLength];
+    for(int i = 0; i < ArrayLength; i++)
+    {
+        Console.Write($"Введите элемент #{i+1}: ");
+        Array[i] = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("");
+    }
+    return Array;
+}
+int[] array = GetNum("M");
+int result = AboveZeroNumsCount(array);
+Console.WriteLine($"Кол-во чисел больше нуля = {result}");
